197b206edd49f2e2304b175a12b7b390f15431a9
amit_qlogy <amit.j.joglekar@gmail.com>
2013-11-21 13:09:35 +0530

    Initial commit

---
+++ src/example/HelloWorldClient.java
@@ -0,0 +1,9 @@
+package example;/**
+ * Created by qlogy on 11/21/13.
+ */
+public class HelloWorldClient {
+  public static void main(String[] argv) {
+      System.out.println("Inside Client");
+
+  }
+}
---
+++ src/example/Path.java
@@ -0,0 +1,7 @@
+package example;
+
+/**
+ * Created by qlogy on 11/21/13.
+ */
+public @interface Path {
+}
